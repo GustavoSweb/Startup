f79bbe154bb396aa022dfa31ee6cc91a0abdea63
Gustavo <gustavodasilvama10@gmail.com>
2024-10-12 13:40:39 -0300

    Session main finishild

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,12 +2,13 @@ const MenuMobile = document.querySelector("#MenuMobile");
 const MenuLinks = document.querySelector("#MenuLinks");
 
 MenuMobile.addEventListener("click", () => {
+  MenuMobile.classList.toggle("menuOnAnimation");
   if (MenuLinks.classList.contains("active")) {
     MenuLinks.classList.remove("active");
 
     setTimeout(() => {
       MenuLinks.style.display = "none";
-    }, 300); // 300ms é o tempo da transição no CSS
+    }, 800); // 300ms é o tempo da transição no CSS
   } else {
     MenuLinks.style.display = "flex"; // Mostra o menu imediatamente
 
